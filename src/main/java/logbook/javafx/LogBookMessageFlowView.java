31c475b1245d518af19db8c0130258d87640491a
sakura0689 <10571179+sakura0689@users.noreply.github.com>
2025-02-23 20:48:36 +0900

    [fix]DevToolsが落ちた際対応 ステータスアイコンを追加、切断時点滅するアニメーションを追加

--- src/main/java/logbook/javafx/LogBookMessageFlowView.java
+++ src/main/java/logbook/javafx/LogBookMessageFlowView.java
@@ -5,9 +5,13 @@ import java.util.Map;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import logbook.WebSocketStatus;
@@ -20,6 +24,9 @@ public class LogBookMessageFlowView extends Application {
     private Label portLabel;
     private Label webSocketStatusLabel;
     private Label queueStatusLabel;
+    private Circle webSocketStatusCircle;
+    private Timeline blinkTimeline; // 点滅アニメーション
+    private boolean isInitConnect = false;
     
     @Override
     public void start(Stage primaryStage) {
@@ -27,6 +34,7 @@ public class LogBookMessageFlowView extends Application {
         portLabel = new Label("起動Port:" + config.getKoukainissikaiMessageFlowPort() + " 航海日誌改Port" + config.getKoukainissikaiPort());
 
         webSocketStatusLabel = new Label();
+        webSocketStatusCircle = new Circle(6);
         updateWebSocketStatus(); // 初回表示
         
         queueStatusLabel = new Label();
@@ -43,9 +51,13 @@ public class LogBookMessageFlowView extends Application {
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
         
+        HBox webSocketStatusBox = new HBox(10, webSocketStatusLabel, webSocketStatusCircle);
+        webSocketStatusBox.setAlignment(Pos.CENTER_LEFT);
+        
         VBox root = new VBox(10);
-        root.getChildren().addAll(portLabel, webSocketStatusLabel, queueStatusLabel);
-
+        root.getChildren().addAll(portLabel, webSocketStatusBox, queueStatusLabel);
+        root.setAlignment(Pos.CENTER_LEFT);
+        
         Scene scene = new Scene(root, 400, 100);
         primaryStage.setScene(scene);
         primaryStage.setTitle("航海日誌改 MessageFlow");
@@ -59,6 +71,18 @@ public class LogBookMessageFlowView extends Application {
 
         webSocketStatusLabel.setText(String.format("WebSocket 接続数 API : %d Image : %d ImageJson : %d",
                 apiCount, imageCount, imageJsonCount));
+        
+        if (apiCount > 0 || imageCount > 0 || imageJsonCount > 0) {
+            webSocketStatusCircle.setFill(Color.GREEN);
+            isInitConnect = true;
+            stopBlinking();
+        } else {
+            if (isInitConnect) {
+                startBlinking();
+            } else {
+                webSocketStatusCircle.setFill(Color.RED); 
+            }
+        }
     }
     
 
@@ -73,6 +97,30 @@ public class LogBookMessageFlowView extends Application {
                 apiCount, imageCount, imageJsonCount));
     }
 
+    /**
+     * アイコンの点滅処理
+     */
+    private void startBlinking() {
+        if (blinkTimeline == null) {
+            blinkTimeline = new Timeline(
+                new KeyFrame(Duration.seconds(1.0), e -> webSocketStatusCircle.setFill(Color.RED)),
+                new KeyFrame(Duration.seconds(1.5), e -> webSocketStatusCircle.setFill(Color.TRANSPARENT))
+            );
+            blinkTimeline.setCycleCount(Timeline.INDEFINITE);
+        }
+        blinkTimeline.play();
+    }
+
+    /**
+     * 点滅停止処理
+     */
+    private void stopBlinking() {
+        if (blinkTimeline != null) {
+            blinkTimeline.stop();
+            webSocketStatusCircle.setFill(Color.GREEN);
+        }
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
