58f8b4692484677795f97438dbba1f16ec514001
sakura0689 <10571179+sakura0689@users.noreply.github.com>
2025-10-18 08:27:30 +0900

    [fix]送信先ポートを航海日誌改のデフォルトである8888に変更

--- dist-includes/launch-messageflow.bat
+++ dist-includes/launch-messageflow.bat
@@ -4,4 +4,4 @@ chcp 65001 >nul
 SET JVM_OPT=-XX:MaxMetaspaceSize=256M
 
 REM アプリケーションを起動
-START javaw %JVM_OPT% -Dport=8890 -Dkaiport=8891 -jar logbook-kai-messageflow.jar
+START javaw %JVM_OPT% -Dport=8890 -Dkaiport=8888 -jar logbook-kai-messageflow.jar
